4c4395fc889f598dc265b31e8aaa6fe5c266ab84
ekaterina <ekaterina0853@mail.ru>
2012-10-04 22:10:18 +0300

    Added apple class

--- js/scene.js
+++ js/scene.js
@@ -3,6 +3,7 @@ var hero;
 var apples = [];
 var width;
 var height;
+var tempAppleRadius = 10; //TODO Remove this
 
 // ---------------------
 
@@ -32,5 +33,27 @@ function Hero(x, y){
         {
             this.y += 10;
         }
+    };
+}
+
+// Our Apple (or Acorn)
+// TODO Maybe this method must be renamed
+function Apple(x, y){
+    this.x = x;
+    this.y = y;
+    this.r = 0;
+    this.g = 255;
+    this.b = 0;
+    this.isFalling = false;
+    this.speed = 5+Random(7);
+
+    // Drawing one specified apple
+    this.drawApple = function (ctx)
+    {   var color = Color(this.r, this.g, this.b);
+        ctx.fillStyle = color;
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, tempAppleRadius, 0, Math.PI*2, true);
+        ctx.closePath();
+        ctx.fill();
     };
 }
