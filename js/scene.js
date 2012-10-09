c9699e5c79b5b47c352822abff36c2dc3313c6c1
Kirill Buga <tztimezero@yandex.ru>
2012-10-09 18:11:43 +0300

    Added basic hero action

--- js/scene.js
+++ js/scene.js
@@ -17,35 +17,51 @@ function Hero(x, y){
     this.x = x;
     this.y = y;
     this.IsRunning = false;
+    this.SpeedY = 0;
 
     // Drawing hero
-    this.drawHero = function (ctx)
+    this.drawHero = function (ctx, hero)
     {
         currentFrame = ++currentFrame%9;
 
-        var runTexture = new Image();
-        runTexture.src = 'img/Run.png';
-        runTexture.onload = function() {
-            if( !this.IsRunning )
-                ctx.drawImage (runTexture, 34 * currentFrame, 0, 34, 42, this.x, this.y, 34, 42);
+        var runLeftTexture = new Image();
+        runLeftTexture.src = 'img/RunLeft.png';
+        runLeftTexture.onload = function() {
+            if( hero.IsRunning && direction == 'left')
+                ctx.drawImage (runLeftTexture, 34 * currentFrame, 0, 34, 42, hero.x, hero.y, 34, 42);
+        };
+
+        var runRightTexture = new Image();
+        runRightTexture.src = 'img/RunRight.png';
+        runRightTexture.onload = function() {
+            if( hero.IsRunning && direction == 'right')
+                ctx.drawImage (runRightTexture, 34 * currentFrame, 0, 34, 42, hero.x, hero.y, 34, 42);
         };
 
         var idleTexture = new Image();
         idleTexture.src = 'img/Idle.png';
         idleTexture.onload = function() {
-            if( this.IsRunning )
-                ctx.drawImage(idleTexture, 0, 0, 34, 42, this.x, this.y, 34, 42);
+            if( !hero.IsRunning )
+                ctx.drawImage(idleTexture, 0, 0, 34, 42, hero.x, hero.y, 34, 42);
         };
+
     };
 
     // hero gravity
     this.applyGravity = function ()
     {
-        if ((this.y + 20) < height)
+        if ((this.y + 40) < height)
         {
             this.y += 10;
         }
+
+        if(this.SpeedY > 0) this.SpeedY -=0.5;
     };
+
+    this.Jump = function()
+    {
+        this.SpeedY = 20;
+    }
 }
 
 // Our Apple (or Acorn)
@@ -155,13 +171,16 @@ function appleRipening(){
 
 // hero moving
 function move(direction){
-    if(direction == 'left' && hero.x > 0)
-    {
-        hero.x -= 5;
-    }
-    if(direction == 'right' && hero.x < width)
-    {
-        hero.x += 5;
+    hero.y -= hero.SpeedY;
+    if(hero.IsRunning){
+        if(direction == 'left' && hero.x > 0)
+        {
+            hero.x -= 10;
+        }
+        if(direction == 'right' && hero.x < width - 30)
+        {
+            hero.x += 10;
+        }
     }
 }
 
@@ -179,7 +198,7 @@ function drawScene(){
     drawAllApples(ctx);
     appleRipening();
     appleFalling();
-    hero.drawHero(ctx);
+    hero.drawHero(ctx, hero);
     hero.applyGravity();
     move(direction);
 }
@@ -193,7 +212,7 @@ $(function(){
     width = canvas.width;
     height = canvas.height;
 
-    hero = new Hero(100,100);
+    hero = new Hero(300,300);
 
     var applesCount = 1;
     currentFrame = 0;
@@ -203,6 +222,21 @@ $(function(){
         newApple();
     }
 
+    canvas.onmousedown = function(e){
+        var x = e.offsetX;
+        if(x > width / 2) direction = 'right';
+        else direction = 'left';
+        hero.IsRunning = true;
+    }
 
-    setInterval(drawScene, 30);
+    canvas.onmouseup = function(e){
+        hero.IsRunning = false;
+    }
+
+    canvas.ondblclick = function (e){
+        hero.Jump();
+    }
+
+
+    setInterval(drawScene, 40);
 });
