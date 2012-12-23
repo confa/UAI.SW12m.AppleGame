cffc00aa50f0bd97997a5ed67dbcacac40853b49
sergey <mishustinsergey@gmail.com>
2012-12-23 19:01:23 +0200

    Remove footer from awards

--- js/scene.js
+++ js/scene.js
@@ -206,6 +206,7 @@ function Initialization()
     var newHeroY = 500;
 
     hero = new Hero(newHeroX, newHeroY);
+    hero.HP=5;
 
     jQuery('#gameInfo').removeAttr('hidden');
     jQuery('#invulnerability').removeAttr('disabled');
